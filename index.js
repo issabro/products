737ad1dcba43b722c6308d0808a47c4d320528e3
issa <ne211227@senshu-u.jp>
2023-06-08 23:36:58 +0900

    何もわからん

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
 const express = require('express');
+const https = require('https');
 
 const PORT = 8080;
 
-let stock = 5;
+let URL = 'https://express.heartrails.com/api/json?method=getStations&line=東急田園都市線';
 
 const app = express();
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-  res.render('top.ejs', { stock: stock });
+app.get('/', (req, expressResult) => {
+  https.get(URL, httpResult => {
+    let body = '';
+    httpResult.on('data', chunk => {
+      body += chunk;
+    });
+    httpResult.on('end', res => {
+      stationsInLine = JSON.parse(body).response.station;
+      let stations = stationsInLine.map(s => s.name);
+      expressResult.render('top.ejs', { stations: stations });
+    });
+  });
 });
 
-app.post('/order', (req, res) => {
-  let stock_check = stock - Number(req.body.order);
-  console.log(Number(req.body.order));
-  console.log(stock_check);
-  if (stock_check < 0) {
-    res.render('out_of_stock.ejs');
+class Product {
+  constructor(id, name, price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
   }
-  else {
-    stock = stock_check;
-    res.render('order_succeeded.ejs',
-      { order: Number(req.body.order) }
-    );
+}
+
+let foods = new ProductDB();
+
+foods.add("ポッキー", 280);
+foods.add("きのこの山", 190);
+foods.add("たけのこの里", 190);
+
+class ProductDB {
+  constructor() {
+    this.productNum = 0;
+    this.products = [];
   }
-});
+
+  add(name, price) {
+    this.products.push(new Product(this.productNum, name, price));
+    this.productNum++;
+  }
+}
 
 app.listen(process.env.PORT || PORT);
